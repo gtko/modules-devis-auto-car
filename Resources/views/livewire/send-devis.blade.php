b06b15705152b023da52f97ad72596015c1a8891
aurelien3333 <aurelienverdeau@gmail.com>
2021-11-16 15:54:49 +0100

    fiw btn valider devis

--- Resources/views/livewire/send-devis.blade.php
+++ Resources/views/livewire/send-devis.blade.php
@@ -1,6 +1,6 @@
 <div>
     <x-basecore::loading-replace>
-          <span class="cursor-pointer" wire:click="send">
+          <span class="cursor-pointer" wire:click="send" title="envoyer le devis">
             @icon('email', null, 'w-4 h-4 mr-1')
           </span>
     </x-basecore::loading-replace>
