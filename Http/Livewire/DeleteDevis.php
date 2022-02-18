0acc46f809529ec7c196f0d5068be64e9e34d0ab
aurelien3333 <aurelienverdeau@gmail.com>
2022-02-18 11:48:39 +0100

    add delete devis soft + timeline

--- Http/Livewire/DeleteDevis.php
+++ Http/Livewire/DeleteDevis.php
@@ -2,8 +2,12 @@
 
 namespace Modules\DevisAutoCar\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Modules\CoreCRM\Contracts\Repositories\DevisRepositoryContract;
+use Modules\CoreCRM\Flow\Attributes\ClientDossierDevisDelete;
+use Modules\CoreCRM\Flow\Attributes\ClientDossierNoteCreate;
+use Modules\CoreCRM\Services\FlowCRM;
 use Modules\DevisAutoCar\Models\Devi;
 
 class DeleteDevis extends Component
@@ -18,7 +22,10 @@ class DeleteDevis extends Component
     public function delete()
     {
         app(DevisRepositoryContract::class)->delete($this->devis);
+        (new FlowCRM())->add($this->devis->dossier, new ClientDossierDevisDelete($this->devis, Auth::user()));
+        $this->emit('refreshDevi');
     }
+
     public function render()
     {
         return view('devisautocar::livewire.delete-devis');
