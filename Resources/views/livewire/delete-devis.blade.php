805f915fe4722ca8a7559534ea2dc6cec6faa877
gtko <gtko@users.noreply.github.com>
2022-02-24 15:16:05 +0100

    Interception de la proformat

--- Resources/views/livewire/delete-devis.blade.php
+++ Resources/views/livewire/delete-devis.blade.php
@@ -1,3 +1,5 @@
-<span title="supprimer le devis" wire:click="delete()" class="cursor-pointer">
+<x-basecore::loading-replace wire:target="delete()">
+    <span title="supprimer le devis" wire:click="delete()" class="cursor-pointer">
             @icon('delete', null, 'w-4 h-4 mr-1')
-        </span>
+    </span>
+</x-basecore::loading-replace>
--- Resources/views/livewire/dupluicate-devis.blade.php
+++ Resources/views/livewire/dupluicate-devis.blade.php
@@ -1,3 +1,8 @@
 <div>
-<span wire:click="duplicate" class="cursor-pointer" title="dupliquer le devis">@icon('addCircle', null, 'w-4 h-4 mr-1')</span>
+    <x-basecore::loading-replace wire:target="duplicate">
+        <span wire:click="duplicate" class="cursor-pointer" title="dupliquer le devis">
+            @icon('addCircle', null, 'w-4 h-4 mr-1')
+        </span>
+    </x-basecore::loading-replace>
+
 </div>
