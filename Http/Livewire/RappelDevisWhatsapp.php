b85396a2d47a5d1062a9fc78aeefe098fb1b3075
aurelien3333 <aurelienverdeau@gmail.com>
2022-03-24 15:49:19 +0100

    add saut de ligne dans le message rapel whatsapps

--- Http/Livewire/RappelDevisWhatsapp.php
+++ Http/Livewire/RappelDevisWhatsapp.php
@@ -33,19 +33,19 @@ class RappelDevisWhatsapp extends Component
         $phone = substr($phone, 1);
         $phone = '33' . $phone;
 
-        $signature = $this->dossier->commercial->format_nom ."\n" . $this->dossier->commercial->email."\n" . $this->dossier->commercial->phone;
-        $text = <<<TEXT
-Bienvenue chez Centrale Autocar.
-J'ai bien reçu votre recherche de transfert en autocar, cependant je n'ai pas réussi à vous joindre afin d’affiner votre projet.
+        $signature = $this->dossier->commercial->format_nom ."\n" . $this->dossier->commercial->email."%0D%0A" . $this->dossier->commercial->phone;
+        $text = "
+Bienvenue chez Centrale Autocar %0D%0A
 
-A quel moment pouvons nous fixer ensemble un RDV téléphonique?
+J'ai bien reçu votre recherche de transfert en autocar, cependant je n'ai pas réussi à vous joindre afin d’affiner votre projet. %0D%0A
 
-D'ici là, je vous invite à me faire part de vos critères par whatsapp  afin que je puisse avancer dans mes recherches :
+A quel moment pouvons nous fixer ensemble un RDV téléphonique? %0D%0A
 
-Je me ferai un plaisir de vous renseigner.
+D'ici là, je vous invite à me faire part de vos critères par whatsapp  afin que je puisse avancer dans mes recherches : %0D%0A
 
-$signature
-TEXT;
+Je me ferai un plaisir de vous renseigner.  %0D%0A"
+
+. $signature;
 
         $lien = 'https://api.whatsapp.com/send?phone=' . $phone . '&text=' . $text;
 
